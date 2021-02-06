0903b60256eb6a2d254533f2f510eff08f8802bd
nison79 <georgenison@gmail.com>
2021-02-06 12:10:12 +0200

    Arkanoid DONE

--- Arkanoid/script.js
+++ Arkanoid/script.js
@@ -105,6 +105,67 @@ function movePaddle() {
   }
 }
 
+//Move Ball on canvas
+function moveBall() {
+  ball.x += ball.dx;
+  ball.y += ball.dy;
+
+  //wall collision (right-left)
+  if(ball.x + ball.size > canvas.width || ball.x - ball.size <0) {
+    ball.dx *= -1 //ball.dx = ball.dx * -1
+  }
+  //wall collision (top-bottom)
+  if(ball.y + ball.size > canvas.height || ball.y - ball.size <0){
+    ball.dy *= -1;
+  }
+  //paddle collision
+  if(
+    ball.x - ball.size > paddle.x && 
+    ball.x + ball.size <paddle.x +paddle.w &&
+    ball.y +ball.size > paddle.y) {
+    ball.dy = -ball.speed;
+  }
+
+  //brick collision
+  bricks.forEach(column => {
+    column.forEach(brick =>{
+      if(brick.visible) {
+        if(ball.x - ball.size > brick.x &&
+          ball.x + ball.size < brick.x + brick.w &&
+          ball.y + ball.size > brick.y &&
+          ball.y - ball.size < brick.y + brick.h
+          ){
+          ball.dy *= -1;
+          brick.visible = false;
+
+          increaseScore();
+      }
+    }
+  });
+  });
+  //Hit bottom wall Loseeeee
+  if(ball.y + ball.size > canvas.height) {
+    showAllBricks();
+    score =0;
+  }
+}
+
+//Increase score
+function increaseScore() {
+  score++;
+
+  if(score % (brickRowCount % brickRowCount) === 0) {
+    showAllBricks(); 
+  }
+}
+
+//make all bricks appear
+function showAllBricks() {
+  bricks.forEach(column => {
+    column.forEach(brick => brick.visible = true)
+  })
+}
+
 //Draw everything
 function draw() {
   //clear canvas
@@ -119,6 +180,7 @@ function draw() {
 //Update canvas drawing and animation
 function update() {
   movePaddle();
+  moveBall();
 
   //Draw everything
   draw();
